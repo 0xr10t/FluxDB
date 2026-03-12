189a4d10c3022a9c6c620ebdf8bb66ab96857512
Abhivansh <31abhivanshj@gmail.com>
2026-03-12 22:40:44 +0530

    add errors and structs

--- crates/storage/src/wal.rs
+++ crates/storage/src/wal.rs
@@ -1,47 +1,97 @@
-//! Functionality for the Write Ahead Log
-//! Must be durable and atomic
-//! Should be able to logs to reconstruct db state
+use crc32fast::Hasher;
+use std::fs::{File, OpenOptions};
+use std::io::{self, BufReader, BufWriter, Read, Write};
+use std::path::{Path, PathBuf};
+use std::time::SystemTime;
+use std::fmt::{Display, Formatter};
 
-use crate::disk::PageId;
-use std::path::PathBuf;
-
-type Lsn = u64;
+use crate::page::Lsn;
 
 #[derive(Debug)]
-pub enum WalError {}
+pub enum WalError {
+    Io(io::Error),
+    CorruptedLog(String),
+    InvalidLsn,
+}
+
+impl From<io::Error> for WalError {
+    fn from(err: io::Error) -> Self {
+        WalError::Io(err)
+    }
+}
+
+impl Display for WalError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            WalError::Io(err) => write!(f, "IO error: {}", err),
+            WalError::CorruptedLog(msg) => write!(f, "Corrupted log: {}", msg),
+            WalError::InvalidLsn => write!(f, "Invalid LSN"),
+        }
+    }
+}
+
+impl std::error::Error for WalError {}
 
 pub type Result<T> = std::result::Result<T, WalError>;
 
-pub enum LogRecord {
-    /// Update to a single page (physical logging).
-    PageUpdate {
-        page_id: PageId,
-        before: Vec<u8>,
-        after: Vec<u8>,
-    },
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum WalEntryType {
+    Put = 0,
+    Delete = 1,
+}
 
-    /// Optional: allocate/free pages, etc.
-    PageAllocate { page_id: PageId },
+impl TryFrom<u8> for WalEntryType {
+    type Error = WalError;
+    fn try_from(value: u8) -> Result<Self> {
+        match value {
+            0 => Ok(WalEntryType::Put),
+            1 => Ok(WalEntryType::Delete),
+            _ => Err(WalError::CorruptedLog(format!("Invalid entry type: {}", value))),
+        }
+    }
+}
+
+pub struct WalEntry {
+    pub lsn: Lsn,
+    pub entry_type: WalEntryType,
+    pub key: Vec<u8>,
+    pub value: Option<Vec<u8>>,
+    pub timestamp: u64,
+}
+
+pub struct WalIterator {
+    reader: BufReader<File>,
 }
 
 pub struct Wal {
-    db_path: PathBuf, // Db file path
-    logs: Vec<LogRecord>,
+    path: PathBuf,
+    file: BufWriter<File>,
+}
+
+impl Iterator for WalIterator {
+    type Item = Result<WalEntry>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!()
+    }
+}
+
+impl WalIterator {
+    pub fn new(path: &Path) -> io::Result<WalIterator> {
+        let file = OpenOptions::new().read(true).open(path)?;
+        Ok(WalIterator {
+            reader: BufReader::new(file),
+        })
+    }
 }
 
 impl Wal {
-    // Append log to Wal
-    // Add checksum to end of log (hash of log)
-    pub fn append() -> Result<()> {
+    pub fn append(&mut self) -> Result<Lsn> {
         todo!()
     }
 
-    // Sync the Wal to disk
-    // Ensure WAL is durable upto lsn
-    //* Should be atomic */
-    // Called before writing dirty page from buffer pool to disk
-    // make
-    pub fn flush(&self, _lsn: Lsn) {
+    pub fn flush(&mut self) -> Result<()> {
         todo!()
     }
 }
+

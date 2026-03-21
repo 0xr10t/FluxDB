f4dc1ccb232dafc0e8ef581e9e2e92542c50bc24
Aryan Baranwal <aryanbaranwal131214@gmail.com>
2026-03-21 09:55:43 +0530

    feat: add DiskManager::new for opening/creating database files

--- crates/storage/src/disk.rs
+++ crates/storage/src/disk.rs
@@ -12,7 +12,15 @@ use std::os::unix::fs::FileExt;
 
 pub type PageId = u64;
 
-pub enum DiskError {}
+pub enum DiskError {
+    Io(io::Error),
+}
+
+impl From<io::Error> for DiskError {
+    fn from(err: io::Error) -> Self {
+        DiskError::Io(err)
+    }
+}
 
 pub type Result<T> = std::result::Result<T, DiskError>;
 
@@ -29,7 +37,6 @@ pub struct DiskManager {
 
 impl DiskManager {
     // Functions to:
-    // - Open or create the database file.
     // - Read a page into a new Vec<> or [u8; page_size].
     // - Write a page from bytes (must be page_size). Durable if you call `sync_data()` afterwards.
     // - sync_data() - Flush file data (Linux: similar to fdatasync via Rust's sync_data).
@@ -38,5 +45,23 @@ impl DiskManager {
     // - fsync temp
     // - rename over destination (atomic on same filesystem)
     // - fsync parent directory
+
+    // - Open or create the database file.
+    pub fn new(path: impl AsRef<Path>, page_size: usize) -> Result<Self> {
+        let file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            // make .write implicit behavior explicit
+            .truncate(false)
+            .open(&path)?;
+
+        Ok(Self {
+            db_path: path.as_ref().to_path_buf(),
+            file,
+            page_size,
+        })
+    }
+
     pub fn sync_file_and_dir(file: &File, file_path: &Path) {}
 }
